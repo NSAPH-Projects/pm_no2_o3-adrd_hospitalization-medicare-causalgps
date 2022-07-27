ccd1b7db8de28ffda867b5a1cf056aec605ef983
Michelle Qin <mqin@holy7c22107.rc.fas.harvard.edu>
2022-07-27 22:01:22 +0000

    remove zip weighting

--- analysis/explore_covariate_distributions.R
+++ analysis/explore_covariate_distributions.R
@@ -6,66 +6,123 @@ gc()
 library(data.table)
 library(fst)
 library(purrr)
-# library(NSAPHutils)
 
 setDTthreads(threads = 16)
 
-dir_data <- "~/nsaph_projects/pm_no2_o3-adrd_hosp-medicare-causalgps/"
-ADRD_agg_unlagged <- read_fst(paste0(dir_data, "data/analysis/ADRD_complete.fst"), as.data.table = TRUE)
+dir_proj <- "~/nsaph_projects/pm_no2_o3-adrd_hosp-medicare-causalgps/"
+dir_data <- paste0(dir_proj, "data/")
+ADRD_agg <- read_fst(paste0(dir_data, "analysis/ADRD_complete.fst"), as.data.table = TRUE)
 
 # Approximate first ADRD shospitalization by requiring no ADRD hosps for 2 years
-ADRD_agg <- ADRD_agg_unlagged[ADRD_year - ffs_entry_year >= 2, ]
+ADRD_agg_lagged <- ADRD_agg[ADRD_year - ffs_entry_year >= 2, ]
 
 
-##### 1. Explore covariate distributions at individual level #####
+##### 1. Examine aggregated covariates #####
+
+table(ADRD_agg_lagged$statecode) # Not in dataset: AK, HI, PR
+
+
+##### 2. Explore covariate distributions at individual level #####
+
+disaggregate <- function(var){
+  return(unlist(map2(ADRD_agg_lagged[[var]], ADRD_agg_lagged$n_persons, rep))) 
+}
+
+# Check correlations
+ADRD_agg_lagged[, entry_age := ADRD_age - ADRD_year + ffs_entry_year]
+entry_age <- unlist(map2(ADRD_agg_lagged$entry_age, ADRD_agg_lagged$n_persons, rep))
+ADRD_age <- unlist(map2(ADRD_agg_lagged$ADRD_age, ADRD_agg_lagged$n_persons, rep))
+cor(entry_age, ADRD_age)
+set.seed(200)
+random_rows <- sample(1:nrow(ADRD_agg_lagged), 10000)
+plot(entry_age[random_rows], ADRD_age[random_rows])
+# cor(disaggregate("entry_age"), disaggregate("ADRD_age"))
+# plot(disaggregate("entry_age"), disaggregate("ADRD_age"))
+ADRD_agg_lagged[, entry_age := NULL]
 
 # Explore categorical variables used to stratify
-cat("Number of individuals:", sum(ADRD_agg$n_persons))
-cat("Number of male individuals:", sum(ADRD_agg$n_persons * ADRD_agg$sexM))
-cat("Number of Medicaid-eligible individuals:", sum(ADRD_agg$n_persons * ADRD_agg$any_dual))
-cat("Number of ADRD hospitalizations:", sum(ADRD_agg$n_ADRDhosp))
+cat("Number of individuals:", sum(ADRD_agg_lagged$n_persons))
+cat("Number of male individuals:", sum(ADRD_agg_lagged$n_persons * ADRD_agg_lagged$sexM))
+cat("Number of Medicaid-eligible individuals:", sum(ADRD_agg_lagged$n_persons * ADRD_agg_lagged$any_dual))
+cat("Number of ADRD hospitalizations:", sum(ADRD_agg_lagged$n_ADRDhosp))
 print("Race:")
-print(prop.table(table(unlist(map2(ADRD_agg$race_cat, ADRD_agg$n_persons, rep)))))
+print(prop.table(table(unlist(map2(ADRD_agg_lagged$race_cat, ADRD_agg_lagged$n_persons, rep)))))
 print("Region:")
-print(prop.table(table(unlist(map2(ADRD_agg$region, ADRD_agg$n_persons, rep)))))
+print(prop.table(table(unlist(map2(ADRD_agg_lagged$region, ADRD_agg_lagged$n_persons, rep)))))
+cat("Number of individuals 67-69 yrs old at ADRD hosp/censoring:", sum(ADRD_agg_lagged$n_persons * (ADRD_agg_lagged$ADRD_age >= 67 & ADRD_agg_lagged$ADRD_age <= 69)), "\n")
+cat("Number of individuals 70-74 yrs old at ADRD hosp/censoring:", sum(ADRD_agg_lagged$n_persons * (ADRD_agg_lagged$ADRD_age >= 70 & ADRD_agg_lagged$ADRD_age <= 74)), "\n")
+cat("Number of individuals 75-79 yrs old at ADRD hosp/censoring:", sum(ADRD_agg_lagged$n_persons * (ADRD_agg_lagged$ADRD_age >= 75 & ADRD_agg_lagged$ADRD_age <= 79)), "\n")
+cat("Number of individuals 80-84 yrs old at ADRD hosp/censoring:", sum(ADRD_agg_lagged$n_persons * (ADRD_agg_lagged$ADRD_age >= 80 & ADRD_agg_lagged$ADRD_age <= 84)), "\n")
+cat("Number of individuals 85-89 yrs old at ADRD hosp/censoring:", sum(ADRD_agg_lagged$n_persons * (ADRD_agg_lagged$ADRD_age >= 85 & ADRD_agg_lagged$ADRD_age <= 89)), "\n")
+cat("Number of individuals 90-94 yrs old at ADRD hosp/censoring:", sum(ADRD_agg_lagged$n_persons * (ADRD_agg_lagged$ADRD_age >= 90 & ADRD_agg_lagged$ADRD_age <= 94)), "\n")
+cat("Number of individuals 95+ yrs old at ADRD hosp/censoring:", sum(ADRD_agg_lagged$n_persons * (ADRD_agg_lagged$ADRD_age >= 95)))
 
 # Plot quantitative variables used to stratify
-hist(unlist(map2(ADRD_agg$ADRD_year, ADRD_agg$n_persons, rep)), main="Year of event", xlab="")
-hist(unlist(map2(ADRD_agg$ffs_entry_year, ADRD_agg$n_persons, rep)), main = "FFS entry year", xlab="")
-hist(unlist(map2(ADRD_agg$n_years, ADRD_agg$n_persons, rep)), main = "Number of years in study", xlab="")
+hist(ADRD_agg_lagged$ADRD_year, main="Year of event", xlab="")
+hist(ADRD_agg_lagged$ffs_entry_year, main = "FFS entry year", xlab="")
+hist(ADRD_agg_lagged$n_years, main = "Number of years in study", xlab="")
 
-# Plot quantitative covariates, weighting by strata size #
-hist(unlist(map2(ADRD_agg$mean_bmi, ADRD_agg$n_persons, rep)), main = "Mean BMI in ZIP code in year before event", xlab="")
-hist(unlist(map2(ADRD_agg$smoke_rate, ADRD_agg$n_persons, rep)), main = "Smoking rate in ZIP code in year before event", xlab="")
-hist(unlist(map2(ADRD_agg$medhouseholdincome, ADRD_agg$n_persons, rep)), main = "Median household income in ZIP code in year before event", xlab="Dollars")
-hist(unlist(map2(ADRD_agg$education, ADRD_agg$n_persons, rep)), main = "Proportion NOT graduated high school in ZIP code in year before event", xlab="")
-hist(unlist(map2(ADRD_agg$pct_blk, ADRD_agg$n_persons, rep)), main = "Proportion Black in ZIP code in year before event", xlab="")
-hist(unlist(map2(ADRD_agg$hispanic, ADRD_agg$n_persons, rep)), main = "Proportion Hispanic in ZIP code in year before event", xlab="")
-hist(unlist(map2(ADRD_agg$pct_owner_occ, ADRD_agg$n_persons, rep)), main = "Proportion Residents who own their home in ZIP code in year before event", xlab="")
-hist(unlist(map2(ADRD_agg$PIR, ADRD_agg$n_persons, rep)), main = "Mean Poverty-Income Ratio (PIR) in ZIP code in year before event", xlab="")
-hist(unlist(map2(ADRD_agg$pr, ADRD_agg$n_persons, rep)), main = "Mean annual precipitation in year before event", xlab="Inches")
+# Summarize ZIP-code-level covariates
+mean(ADRD_agg_lagged$mean_bmi)
+sd(ADRD_agg_lagged$mean_bmi)
+mean(ADRD_agg_lagged$smoke_rate)
+sd(ADRD_agg_lagged$smoke_rate)
+mean(ADRD_agg_lagged$pct_blk)
+sd(ADRD_agg_lagged$pct_blk)
+mean(ADRD_agg_lagged$hispanic)
+sd(ADRD_agg_lagged$hispanic)
+mean(ADRD_agg_lagged$education)
+sd(ADRD_agg_lagged$education)
+mean(ADRD_agg_lagged$popdensity)
+sd(ADRD_agg_lagged$popdensity)
+mean(ADRD_agg_lagged$poverty)
+sd(ADRD_agg_lagged$poverty)
+mean(ADRD_agg_lagged$medhouseholdincome)
+sd(ADRD_agg_lagged$medhouseholdincome)
+mean(ADRD_agg_lagged$PIR)
+sd(ADRD_agg_lagged$PIR)
+mean(ADRD_agg_lagged$pct_owner_occ)
+sd(ADRD_agg_lagged$pct_owner_occ)
+
+hist(ADRD_agg_lagged$mean_bmi, main = "Mean BMI in ZIP code in year before event", xlab="")
+hist(ADRD_agg_lagged$smoke_rate, main = "Smoking rate in ZIP code in year before event", xlab="")
+hist(ADRD_agg_lagged$medhouseholdincome, main = "Median household income in ZIP code in year before event", xlab="Dollars")
+hist(ADRD_agg_lagged$education, main = "Proportion NOT graduated high school in ZIP code in year before event", xlab="")
+hist(ADRD_agg_lagged$pct_blk, main = "Proportion Black in ZIP code in year before event", xlab="")
+hist(ADRD_agg_lagged$popdensity, main = "Population Density in ZIP code in year before event", xlab="People/Mile^2")
+hist(ADRD_agg_lagged$hispanic, main = "Proportion Hispanic in ZIP code in year before event", xlab="")
+hist(ADRD_agg_lagged$pct_owner_occ, main = "Proportion of residents who own their home in ZIP code in year before event", xlab="")
+hist(ADRD_agg_lagged$poverty, main = "Proportion of residents living below poverty line in year before event", xlab="")
+hist(ADRD_agg_lagged$pir, main = "Median Price-Income Ratio (PIR) in ZIP code in year before event", xlab="")
+hist(ADRD_agg_lagged$pr, main = "Mean annual precipitation in year before event", xlab="Inches")
 
 logit <- function(x){
   return(log(x/(1-x)))
 }
 
 # Plot transformed covariates, weighting by strata size
-hist(sqrt(unlist(map2(ADRD_agg$medhouseholdincome, ADRD_agg$n_persons, rep))), main = "Sqrt(Median household income in ZIP code in year before event)", xlab="Sqrt(Dollars)")
-hist(logit(unlist(map2(ADRD_agg$education, ADRD_agg$n_persons, rep))), main = "Logit(Proportion NOT graduated high school in ZIP code in year before event)", xlab="") # sqrt transformation would also make distribution symmetric
-hist(logit(unlist(map2(ADRD_agg$pct_blk, ADRD_agg$n_persons, rep))), main = "Logit(Proportion Black in ZIP code in year before event)", xlab="")
-hist(logit(unlist(map2(ADRD_agg$hispanic, ADRD_agg$n_persons, rep))), main = "Logit(Proportion Hispanic in ZIP code in year before event)", xlab="")
-hist(logit(unlist(map2(ADRD_agg$pct_owner_occ, ADRD_agg$n_persons, rep))), main = "Logit(Proportion Residents who own their home in ZIP code in year before event)", xlab="")
-hist(log(unlist(map2(ADRD_agg$PIR, ADRD_agg$n_persons, rep))), main = "Log(Mean Poverty-Income Ratio (PIR) in ZIP code in year before event)", xlab="") # skew left after log transformation
+hist(sqrt(ADRD_agg_lagged$medhouseholdincome), main = "Sqrt(Median household income in ZIP code in year before event)", xlab="Sqrt(Dollars)")
+hist(logit(ADRD_agg_lagged$education), main = "Logit(Proportion NOT graduated high school in ZIP code in year before event)", xlab="") # sqrt transformation would also make distribution symmetric
+hist(logit(ADRD_agg_lagged$pct_blk), main = "Logit(Proportion Black in ZIP code in year before event)", xlab="")
+hist(logit(ADRD_agg_lagged$hispanic), main = "Logit(Proportion Hispanic in ZIP code in year before event)", xlab="")
+hist(log(ADRD_agg_lagged$popdensity), main = "Log(Population Density in ZIP code in year before event)", xlab="Log(People/Mile^2)")
+hist(logit(ADRD_agg_lagged$pct_owner_occ), main = "Logit(Proportion of residents who own their home in ZIP code in year before event)", xlab="")
+hist(logit(ADRD_agg_lagged$poverty), main = "Logit(Proportion of residents living below poverty line in ZIP code in year before event)", xlab="")
+hist(log(ADRD_agg_lagged$pir), main = "Log(Mean Poverty-Income Ratio (PIR) in ZIP code in year before event)", xlab="") # skew left after log transformation
 
-# Plot exposure variables (quantitative), weighted by strata size
-hist(unlist(map2(ADRD_agg$pm25, ADRD_agg$n_persons, rep)), main = "Mean annual PM2.5 in ZIP code in year before event", xlab="Micrograms/cubic meter")
-hist(unlist(map2(ADRD_agg$no2, ADRD_agg$n_persons, rep)), main = "Mean annual NO2 in ZOP code in year before event", xlab="PPB") # skew right
-hist(unlist(map2(ADRD_agg$ozone_summer, ADRD_agg$n_persons, rep)), main = "Mean summer ozone in ZIP code in year before event", xlab="PPB")
-
-hist(sqrt(unlist(map2(ADRD_agg$no2, ADRD_agg$n_persons, rep))), main = "Sqrt(Mean annual NO2 in ZOP code in year before event)", xlab="Sqrt(PPB)")
+# Summarize ZIP-level exposures
+mean(ADRD_agg_lagged$pm25)
+sd(ADRD_agg_lagged$pm25)
+mean(ADRD_agg_lagged$no2)
+sd(ADRD_agg_lagged$no2)
+mean(ADRD_agg_lagged$ozone_summer)
+sd(ADRD_agg_lagged$ozone_summer)
+cor(ADRD_agg_lagged$pm25, ADRD_agg_lagged$no2)
+cor(ADRD_agg_lagged$pm25, ADRD_agg_lagged$ozone_summer)
+cor(ADRD_agg_lagged$no2, ADRD_agg_lagged$ozone_summer)
 
 # Save transformed variables in data table
-ADRD_agg[, `:=`(sqrt_medhouseholdincome = sqrt(medhouseholdincome),
+ADRD_agg_lagged[, `:=`(sqrt_medhouseholdincome = sqrt(medhouseholdincome),
                 logit_education = logit(education),
                 logit_pct_blk = logit(pct_blk),
                 logit_hispanic = logit(hispanic),
